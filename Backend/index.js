e63562fe719cc0008a3de704582e2d71f119fa03
Akhil raj <123929609+Akhil007865678@users.noreply.github.com>
2025-06-27 16:49:59 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 dotenv.config();
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: `${process.env.FRONTEND_URL}`,
     credentials: true
 }))
 app.use(express.json());
