81435d71d6bd50557c83208afb599dc2b5394aff
Akhil raj <akhilpintu308@gmail.com>
2025-06-28 12:07:14 +0530

    Fix: Aligned CORS with Render domain

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors({
-    origin: `${process.env.FRONTEND_URL}`,
+    origin: 'https://host1-q2tj.onrender.com',
     credentials: true
 }));
 app.use(express.json());
