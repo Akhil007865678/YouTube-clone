13a7c3898a6e00d429daa3fb17868584fd09230e
Akhil raj <123929609+Akhil007865678@users.noreply.github.com>
2025-10-14 09:42:38 +0530

    Update home.js

--- frontend/src/Pages/Home/home.js
+++ frontend/src/Pages/Home/home.js
@@ -1,4 +1,4 @@
-/*import React from 'react'
+import React from 'react'
 import SideNavbar from '../../components/SideNavbar/sideNavbar';
 import HomePage from '../../components/HomePage/homePage';
 import './home .css';
@@ -15,40 +15,3 @@ const home = ({sideNavbar}) => {
 }
 
 export default home;
-*/
-import React, { useEffect, useState } from 'react';
-import SideNavbar from '../../components/SideNavbar/sideNavbar';
-import HomePage from '../../components/HomePage/homePage';
-import './home.css';
-import BottomNavbar from '../../components/BottumNav/BottumNavbar';
-
-const Home = ({ sideNavbar }) => {
-  const [redirecting, setRedirecting] = useState(true);
-
-  useEffect(() => {
-    // Redirect after 10 seconds
-    const timer = setTimeout(() => {
-      window.location.href = 'https://you-tube-clone-kappa-two.vercel.app';
-    }, 10000); // 10000ms = 10 seconds
-
-    return () => clearTimeout(timer); // cleanup
-  }, []);
-
-  return (
-    <div className='home'>
-      {redirecting && (
-        <div className="redirect-message">
-          <h2>Server is starting...</h2>
-          <p>Redirecting to live frontend in 10 seconds.</p>
-        </div>
-      )}
-      
-      {/* Optional: You can still render the normal home page behind the message */}
-      <SideNavbar sideNavbar={sideNavbar} />
-      <HomePage sideNavbar={sideNavbar} />
-      <BottomNavbar />
-    </div>
-  );
-};
-
-export default Home;
