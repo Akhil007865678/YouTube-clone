77debd179d60c8eb554ccdccd39c7e87e4151e80
Akhil raj <123929609+Akhil007865678@users.noreply.github.com>
2025-10-14 11:04:46 +0530

    Update home.js

--- frontend/src/Pages/Home/home.js
+++ frontend/src/Pages/Home/home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+/*import React from 'react'
 import SideNavbar from '../../components/SideNavbar/sideNavbar';
 import HomePage from '../../components/HomePage/homePage';
 import './home .css';
@@ -15,3 +15,51 @@ const home = ({sideNavbar}) => {
 }
 
 export default home;
+*/
+import React, { useEffect, useState } from 'react';
+import SideNavbar from '../../components/SideNavbar/sideNavbar';
+import HomePage from '../../components/HomePage/homePage';
+import BottomNavbar from '../../components/BottumNav/BottumNavbar';
+import './home .css';
+
+const Home = ({ sideNavbar }) => {
+  const [backendReady, setBackendReady] = useState(false);
+
+  useEffect(() => {
+    const checkBackend = () => {
+      fetch('https://youtube-clone-cuxp.onrender.com') // hit root or any lightweight endpoint
+        .then((res) => {
+          if (res.ok) setBackendReady(true);
+        })
+        .catch(() => setBackendReady(false));
+    };
+
+    // Initial check
+    checkBackend();
+
+    // Poll every 5 seconds until backend is ready
+    const interval = setInterval(() => {
+      checkBackend();
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="home">
+      {!backendReady && (
+        <div className="backend-message">
+          <h2>Backend is waking up...</h2>
+          <p>It may take a few seconds to start. Please wait.</p>
+        </div>
+      )}
+
+      {/* Normal homepage components */}
+      <SideNavbar sideNavbar={sideNavbar} />
+      <HomePage sideNavbar={sideNavbar} />
+      <BottomNavbar />
+    </div>
+  );
+};
+
+export default Home;
