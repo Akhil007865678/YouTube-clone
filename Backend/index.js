51adf804791f1b3dede6c0101a8e5e4921bf6114
Akhil raj <123929609+Akhil007865678@users.noreply.github.com>
2025-10-14 08:31:21 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 app.set('trust proxy', 1);
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.FRONTEND_URL || "https://you-tube-clone-kappa-two.vercel.app",
   credentials: true
 }));
 app.use(express.json());
