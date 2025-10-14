c7e4d4e064a625bc9fcca3c829419d453daa72aa
Akhil raj <123929609+Akhil007865678@users.noreply.github.com>
2025-10-14 10:17:54 +0530

    Update home.js

--- frontend/src/Pages/Home/home.js
+++ frontend/src/Pages/Home/home.js
@@ -1,4 +1,4 @@
-/*import React from 'react'
+import React from 'react'
 import SideNavbar from '../../components/SideNavbar/sideNavbar';
 import HomePage from '../../components/HomePage/homePage';
 import './home .css';
@@ -15,31 +15,3 @@ const home = ({sideNavbar}) => {
 }
 
 export default home;
-*/
-import React from 'react';
-import SideNavbar from '../../components/SideNavbar/sideNavbar';
-import HomePage from '../../components/HomePage/homePage';
-import './home .css';
-import BottomNavbar from '../../components/BottumNav/BottumNavbar';
-
-const Home = ({ sideNavbar }) => {
-  return (
-    <div className='home'>
-      {/* Friendly message for recruiters */}
-      <div className="landing-message">
-        <h2>Welcome to YouTube Clone</h2>
-        <p>Our live frontend is deployed on Vercel. Click the link below to visit it:</p>
-        <a href="https://you-tube-clone-kappa-two.vercel.app" target="_blank" rel="noopener noreferrer">
-          Go to Live Frontend
-        </a>
-      </div>
-
-      {/* Normal homepage components */}
-      <SideNavbar sideNavbar={sideNavbar}/>
-      <HomePage sideNavbar={sideNavbar}/>
-      <BottomNavbar />
-    </div>
-  );
-};
-
-export default Home;
